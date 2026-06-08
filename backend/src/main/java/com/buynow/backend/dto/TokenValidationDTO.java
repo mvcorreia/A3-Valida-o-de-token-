f70f0dee0a337cea5d34e73939f1c4ab40242474
mvcorreia <mviniciusneiva@gmail.com>
2026-06-07 23:27:29 -0300

    testes

--- backend/src/main/java/com/buynow/backend/dto/TokenValidationDTO.java
+++ backend/src/main/java/com/buynow/backend/dto/TokenValidationDTO.java
@@ -4,10 +4,20 @@ public class TokenValidationDTO {
 
     private String token;
 
+    // Construtor padrão (Obrigatório para o Jackson funcionar!)
     public TokenValidationDTO() {
+    }
+
+    public TokenValidationDTO(String token) {
+        this.token = token;
     }
 
     public String getToken() {
         return token;
+    }
+
+    // Adicione o Setter se não tiver (Ajuda o Jackson a injetar o valor)
+    public void setToken(String token) {
+        this.token = token;
     }
 }
